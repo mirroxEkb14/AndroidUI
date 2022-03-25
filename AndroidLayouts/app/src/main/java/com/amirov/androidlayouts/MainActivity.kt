c30176dc8651052516184a2765c8a44f7edae240
Daniyar <dancho.rancho07@gmail.com>
2022-03-25 23:07:52 +0100

    units 19.2-19.6, layouts

--- AndroidLayouts/app/src/main/java/com/amirov/androidlayouts/MainActivity.kt
+++ AndroidLayouts/app/src/main/java/com/amirov/androidlayouts/MainActivity.kt
@@ -2,10 +2,19 @@ package com.amirov.androidlayouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // find the container
+        val content = findViewById<ViewGroup>(R.id.content)
+        for (i in 0..100) {
+            // get the current view and add it to the container
+            val view = layoutInflater.inflate(R.layout.scroll_view_item2, content, false)
+            content.addView(view)
+        }
     }
 }
