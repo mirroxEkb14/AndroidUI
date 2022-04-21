64d8c3f32e83b1324f30d39458d73740286a7dfd
Daniyar <dancho.rancho07@gmail.com>
2022-04-21 17:37:47 +0200

    'back' button when switching between Activity and Fragment, added additional comments

--- Navigation/BackNavigation/app/src/main/java/com/amirovdev/backnavigation/MainActivity.kt
+++ Navigation/BackNavigation/app/src/main/java/com/amirovdev/backnavigation/MainActivity.kt
@@ -3,7 +3,6 @@ package com.amirovdev.backnavigation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
@@ -17,6 +16,21 @@ import kotlinx.android.synthetic.main.activity_main.*
  *          1. set out Toolbar as 'support action bar' -> setSupportActionBar(findViewById(R.id.toolbar));
  *          2. tell the app that we want a "back" button -> supportActionBar?.setDisplayHomeAsUpEnabled(true);
  *          3. add Listener for the button -> toolbar.setNavigationOnClickListener { onBackPressed() }
+ *
+ * When navigating in the opposite direction:
+ *      if the Activity has Fragments in stack, in the beginning navigation goes in Fragments,
+ *      only then in the previous Activity
+ *
+ * When switching to another Activity, the Activity is not destroyed
+ * (destroys only with finish() method)
+ *
+ * When navigating in the opposite direction, Activities are destroyed
+ *
+ * What will happen to Activity if it is transparent when switching between Activities?
+ * - Activity will be in onPause(), we can see the first Activity, but there is no cooperation with it
+ * to make Activity transparent: create style with two tags:
+ *      "android:windowIsTranslucent" = true // if the previous Activity will be in onStop() or onPause()
+ *      "android:windowBackground" = @android:color/transparent
  */
 
 class MainActivity : AppCompatActivity() {
