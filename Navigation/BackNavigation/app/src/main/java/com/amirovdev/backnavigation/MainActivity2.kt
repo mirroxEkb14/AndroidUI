204faf9faad42472fd68e3c44264bec94617e077
Daniyar <dancho.rancho07@gmail.com>
2022-04-24 20:41:58 +0200

    unit 26.3: back navigation button, two ways of switching between Fragments

--- Navigation/BackNavigation/app/src/main/java/com/amirovdev/backnavigation/MainActivity2.kt
+++ Navigation/BackNavigation/app/src/main/java/com/amirovdev/backnavigation/MainActivity2.kt
@@ -28,25 +28,32 @@ class MainActivity2 : AppCompatActivity() {
         }
     }
 
+    // When we go to Fragment1, then to Fragment2, then again to Fragment1
+    // and then again to Fragment2, in stack we have 4 Fragments
     override fun onBackPressed() {
-//        if (supportFragmentManager.findFragmentById(R.id.container_activity2)!! == getVisibleFragment()) {
-//            finish()
-//        }
-
-//        val fragments = supportFragmentManager.fragments
-//        if(fragments.last() is BlankFragment1 && fragments.size < 3) {
-//            finish()
-//        }
-
+        /*
+        With the following logic,
+        when we go back from Fragment2(number 4 in stack, the last one),
+        we get to Fragment1(number 3 in stack), then we go back again and get to
+        Fragment2(number 2 in stack), then press a back button again -
+        we get to Fragment1(number 1 in stack), then again back - Activity1
+        */
         val fragments = supportFragmentManager.fragments
-        if (fragments[fragments.size - 1] is BlankFragment1) finish()
+        if(fragments.last() is BlankFragment1 && fragments.size < 3) {
+            finish()
+        } else {
+            super.onBackPressed()
+        }
 
-        super.onBackPressed()
-    }
-
-    private fun getVisibleFragment(): Fragment? {
-        return supportFragmentManager.fragments
-            .filter { it.isVisible }
-            .getOrNull(0)
+        /*
+        With the following logic,
+        when we go back from Fragment2(number 4 in stack, the last one),
+        we get to Fragment1(number 3 in stack), then we go back again and get to Activity1
+        (we leave two Fragments in stack and go immediately to Activity1)
+        */
+        when (supportFragmentManager.findFragmentById(R.id.container_activity2)) {
+            is BlankFragment1 -> finish()
+            else -> super.onBackPressed()
+        }
     }
 }
